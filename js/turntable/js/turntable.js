4238e114a585e6d0bc8daa82156b33eaa126e293
xing liu <ogilhinn@gmail.com>
2016-12-12 11:25:51 +0800

    :zap: js幸运大转盘

--- js/turntable/js/turntable.js
+++ js/turntable/js/turntable.js
@@ -1,7 +1,7 @@
 (function () {
 
-    let prizeItem = [1, 2, 3, 4, 5,6],
-        priseColor = ['#ff5959','#ff7f7e','#ff5959','#ff7f7e','#ff5959','#ff7f7e'],
+    let prizeItem = [1, 2, 3, 4, 5, 6],
+        priseColor = ['#ff5959', '#ff7f7e', '#ff5959', '#ff7f7e', '#ff5959', '#ff7f7e'],
         resultAngle = [];
 
     /**
@@ -11,7 +11,7 @@
         let turntable = $('#turntable')[0],  //$对象转DOM对象
             ctx = turntable.getContext('2d');
         let prizeLen = prizeItem.length,
-            sector = Math.PI * 2/prizeLen;
+            sector = Math.PI * 2 / prizeLen;
 
         // 画圆
         ctx.translate(250, 250)  // 移动中心
@@ -21,36 +21,36 @@
         ctx.fill()
 
         // 画出扇形
-        prizeItem.forEach((item, i)=>{
+        prizeItem.forEach((item, i) => {
             ctx.beginPath()
             ctx.moveTo(0, 0)
-            ctx.arc(0, 0, 240, sector *i, sector + sector*i)
+            ctx.arc(0, 0, 240, sector * i, sector + sector * i)
             ctx.fillStyle = priseColor[i]
             ctx.fill()
 
-            if(i % 2 == 0){
+            if (i % 2 == 0) {
                 ctx.beginPath()
                 ctx.moveTo(0, 0)
-                ctx.arc(0, 0,60, sector *i, sector + sector*i)
+                ctx.arc(0, 0, 60, sector * i, sector + sector * i)
                 ctx.fillStyle = priseColor[1]
                 ctx.fill()
             }
 
             // 画出文字
-            ctx.font="28px Arial";
+            ctx.font = "28px Arial";
             let r = 150,
-                angle = sector*180/Math.PI,
-                x1 = r * Math.cos((angle/2 + angle*i)*Math.PI/180),
-                y1 = r * Math.sin((angle/2 + angle*i)*Math.PI/180);
-            resultAngle.push((angle/2 + angle*i)-angle/2)  // 添加中奖角度到数组
+                angle = sector * 180 / Math.PI,
+                x1 = r * Math.cos((angle / 2 + angle * i) * Math.PI / 180),
+                y1 = r * Math.sin((angle / 2 + angle * i) * Math.PI / 180);
+            resultAngle.push((angle / 2 + angle * i) - angle / 2)  // 添加中奖角度到数组
             // console.log(x1,y1,angle)
             ctx.fillStyle = '#fff'
-            ctx.fillText(item,x1,y1)
+            ctx.fillText(item, x1, y1)
         })
 
         // 画内圆
         ctx.beginPath()
-        ctx.arc(0,0,40,0,2*Math.PI)
+        ctx.arc(0, 0, 40, 0, 2 * Math.PI)
         ctx.fillStyle = '#fff'
         ctx.fill()
 
@@ -61,7 +61,7 @@
      * @returns {number}
      */
     function angleRand() {
-        let rand = Math.floor(Math.random()*(prizeItem.length));
+        let rand = Math.floor(Math.random() * (prizeItem.length));
         return rand
     }
 
@@ -70,15 +70,22 @@
      */
     function rotateHandler() {
         let start = $('#start'),
-            turnTable = $('#turntable');
-        start.on('click',function () {
+            turnTable = $('#turntable'),
+            isbegin = false;
+        start.on('click', function () {
+            //  防止连续点击
+            if (isbegin) {
+                return false
+            }
+            isbegin = true
             let result = angleRand()
             turnTable.animate({
-                rotate: 360*8 - 120 - + resultAngle[result]
+                rotate: 360 * 8 - 120 - +resultAngle[result]  //8 - 转8圈
             }, {
                 duration: 6000,
                 easing: 'easeInOutCubic',
                 complete: function () {
+                    isbegin = false
                     console.log(prizeItem[result])
                     alert(prizeItem[result])
                 }
