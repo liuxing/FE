5e8efbd893d753be6292486094948a831708e708
xing liu <ogilhinn@gmail.com>
2016-12-18 16:02:38 +0800

    :wrench:

--- js/musicPlayer/js/app.js
+++ js/musicPlayer/js/app.js
@@ -2,22 +2,22 @@ $(function () {
 
     let musics = [
         {
-            song:'Five Hundred Miles',
+            song: 'Five Hundred Miles',
             url: 'http://m5.file.xiami.com/506/16506/146691/1658080_4017332_l.mp3',
             singer: 'Justin Timberlake'
         },
         {
-            song:'把悲伤留给自己',
+            song: '把悲伤留给自己',
             url: 'http://m5.file.xiami.com/119/119/449/5384_96212_l.mp3',
             singer: '陈升'
         },
         {
-            song:'当爱已成往事',
+            song: '当爱已成往事',
             url: 'http://m5.file.xiami.com/166/1166/5833/377412_21906_l.mp3',
             singer: '张国荣'
         },
         {
-            song:'野子',
+            song: '野子',
             url: 'http://m5.file.xiami.com/83/2075780083/537396972/1774520623_16916469_l.mp3',
             singer: '苏运莹'
         },
@@ -36,14 +36,14 @@ $(function () {
             list = menu.find('ul');
 
         musics.forEach(function (item, i) {
-            list.append('<li><a href="javascript:void(0);">'+item.song+'<span>'+item.singer+'</span></a></li>')
+            list.append('<li><a href="javascript:void(0);">' + item.song + '<span>' + item.singer + '</span></a></li>')
         })
 
         // 打开音乐列表
         menuBtn.on('click', function () {
-            if($(this).hasClass('icon-caidan')){
+            if ($(this).hasClass('icon-caidan')) {
                 menu.slideDown(100)
-            }else{
+            } else {
                 menu.slideUp(100)
             }
             $(this).toggleClass('icon-caidan icon-menu')
@@ -70,7 +70,7 @@ $(function () {
     function choseMusic(i) {
         musicPause()
         music.src = musics[i].url;
-        $('.player-header h1').html(musics[i].song+'<br><small>'+ musics[i].singer+'</small>')
+        $('.player-header h1').html(musics[i].song + '<br><small>' + musics[i].singer + '</small>')
         musicPlay()
     }
 
@@ -89,48 +89,48 @@ $(function () {
             like = $('.like');
 
         // 播放暂停
-        onOff.on('click',function () {
-            if($(this).hasClass('icon-zanting')){
+        onOff.on('click', function () {
+            if ($(this).hasClass('icon-zanting')) {
                 musicPlay()
-            }else {
+            } else {
                 musicPause()
             }
         })
 
         // 上一首
-        prev.on('click',function () {
+        prev.on('click', function () {
             i--
-            if (i <= -1){
-                i = musicLen-1
+            if (i <= -1) {
+                i = musicLen - 1
             }
             choseMusic(i)
         })
 
         // 下一首
-        next.on('click',function () {
+        next.on('click', function () {
             i++
-            if(i == musicLen){
+            if (i == musicLen) {
                 i = 0
             }
             console.log(i)
             choseMusic(i)
         })
 
-        volume.on('click',function () {
-            if($(this).hasClass('icon-shengyin')){
+        volume.on('click', function () {
+            if ($(this).hasClass('icon-shengyin')) {
                 music.volume = 0
-            }else {
+            } else {
                 music.volume = 1
             }
             $(this).toggleClass('icon-shengyin icon-jingyin')
         })
 
-        like.on('click',function () {
+        like.on('click', function () {
             $(this).toggleClass('icon-diligently icon-xin')
         })
 
         // 进度条
-        progressBar.on('input',progressCtrl)
+        progressBar.on('input', progressCtrl)
     }
 
     let timer;
@@ -143,16 +143,16 @@ $(function () {
         $('.hand').addClass('play')
         music.play()
         // console.log(music.duration)
-        disk.css('webkitAnimationPlayState','running')
+        disk.css('webkitAnimationPlayState', 'running')
         // 进度条
         timer = setInterval(function () {
             // 歌曲时间
-            let totalTime = Math.floor(music.duration/60)+':'+Math.round(music.duration%60);
-            let nowTime = Math.floor(music.currentTime/60)+':'+Math.round(music.currentTime%60)
-            progressBar.val(100*music.currentTime/music.duration)
-            $('.progress time').text(nowTime+'/'+totalTime)
-            progressBar.css('background',`linear-gradient(to right,#666 ${progressBar.val()}%, #222 ${progressBar.val()}%)`)
-        },1000)
+            let totalTime = Math.floor(music.duration / 60) + ':' + Math.round(music.duration % 60);
+            let nowTime = Math.floor(music.currentTime / 60) + ':' + Math.round(music.currentTime % 60)
+            progressBar.val(100 * music.currentTime / music.duration)
+            $('.progress time').text(nowTime + '/' + totalTime)
+            progressBar.css('background', `linear-gradient(to right,#666 ${progressBar.val()}%, #222 ${progressBar.val()}%)`)
+        }, 1000)
     }
 
     /**
@@ -162,7 +162,7 @@ $(function () {
         onOff.removeClass('icon-bofang').addClass('icon-zanting')
         $('.hand').removeClass('play')
         music.pause()
-        disk.css('webkitAnimationPlayState','paused')
+        disk.css('webkitAnimationPlayState', 'paused')
         clearInterval(timer)
     }
 
@@ -171,10 +171,11 @@ $(function () {
      */
     function progressCtrl() {
         let _val = progressBar.val()
-        music.currentTime = _val/100 * music.duration
-        console.log(_val/100 * music.duration)
-        progressBar.css('background',`linear-gradient(to right,#666 ${_val}%, #222 ${_val}%)`)
+        music.currentTime = _val / 100 * music.duration
+        console.log(_val / 100 * music.duration)
+        progressBar.css('background', `linear-gradient(to right,#666 ${_val}%, #222 ${_val}%)`)
     }
+
     // 播放完成自动播放下一首
     music.addEventListener('ended', function () {
         musicPause()
