758583b724b476e0f45448c601bf760ce9b21059
xing liu <ogilhinn@gmail.com>
2016-12-21 11:40:17 +0800

    react life cycle

--- react/todolist_webpack/src/index.js
+++ react/todolist_webpack/src/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './components/Main';
 // import Timer from './components/Timer';
-import MardownEditor from './components/MardownEditor';
+// import MardownEditor from './components/MardownEditor';
+// import LifeCycle from './components/LifeCycle'
+import UserGithub from './components/UserGithub'
 
 // Render the main component into the dom
 ReactDOM.render(
-    <MardownEditor/>,
+    <UserGithub source="https://api.github.com/users/ogilhinn"/>,
     document.getElementById('app')
 );
